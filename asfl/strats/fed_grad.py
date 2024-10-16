91dcaa84bae20868f4c43f2b907b8731410c25ab
Hugh Coleman <46972988+hjc2@users.noreply.github.com>
2024-10-16 14:53:14 -0500

    log

--- asfl/strats/fed_grad.py
+++ asfl/strats/fed_grad.py
@@ -92,7 +92,12 @@ class FedGrad(FedCustom):
 
         metrics_list = [fit_res.parameters for _, fit_res in results]
 
+        weights_results = []
+
         if self.parameter_history is not []:
+            log(CRITICAL, "aggregating parameters")
+            log(INFO, "aggregating parameters")
+
             history_ndarrays = parameters_to_ndarrays(self.parameter_history)
 
             compared_results = [
