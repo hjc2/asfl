62fef9436ed750d4fd8f9816502b423c477fa8ea
Hugh Coleman <46972988+hjc2@users.noreply.github.com>
2024-10-28 00:35:58 -0500

    add get / set

--- asfl/task.py
+++ asfl/task.py
@@ -238,3 +238,12 @@ def test(net, testloader):
             correct += (torch.max(outputs.data, 1)[1] == labels).sum().item()
     accuracy = correct / len(testloader.dataset)
     return loss, accuracy
+
+def get_weights(net):
+    return [val.cpu().numpy() for _, val in net.state_dict().items()]
+
+
+def set_weights(net, parameters):
+    params_dict = zip(net.state_dict().keys(), parameters)
+    state_dict = OrderedDict({k: torch.tensor(v) for k, v in params_dict})
+    net.load_state_dict(state_dict, strict=True)
