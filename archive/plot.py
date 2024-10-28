09f95da9e45ee24bd16b8a85add2b18bfc35256b
Hugh Coleman <46972988+hjc2@users.noreply.github.com>
2024-10-28 12:57:27 -0500

    added newline to labels

--- archive/plot.py
+++ archive/plot.py
@@ -94,7 +94,7 @@ def main():
     # Add YAML configuration information to the plot
     config_text = (f"Num Server Rounds: {num_server_rounds}\n"
                 f"Local Epochs: {local_epochs}\n"
-                f"Num Supernodes: {num_supernodes}"
+                f"Num Supernodes: {num_supernodes}\n"
                 f"Fraction: {fraction}")
     plt.text(0.05, 0.95, config_text, transform=plt.gca().transAxes, 
     fontsize=10, verticalalignment='top', bbox=dict(boxstyle='round,pad=0.5', edgecolor='black', facecolor='lightgrey'))
